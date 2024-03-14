967dd26089fafe321e72bf9cd10b87aa3ca107cc
Nasnast <nasnast@ya.ru>
2024-03-14 12:22:03 +0300

    ProductMashine

---
+++ ProductMashine.java
@@ -0,0 +1,32 @@
+package temp2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public interface ProductMashine {
+
+    public void initProducts();
+
+    public Product getProduct();
+
+    // public List<HotDrink> getList();
+
+    // List<Product> listProduct = new ArrayList<>();
+
+    // public void initProducts(List<Product> listProducts) {
+    // this.listProduct = listProducts;
+    // }
+
+    // public Product getProduct(String name) {
+    // for (Product prod : listProduct) {
+    // if (prod.getName().equals(name))
+    // return prod;
+    // }
+    // return null;
+    // }
+
+    // public List<Product> getList() {
+    // return listProduct;
+
+    // }
+}
